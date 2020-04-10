c658035d45f5b6167a1127eeec1b254b369ddadc
Emre <bayrakemre89@gmail.com>
2020-04-10 12:57:03 +0100

    newUser

--- public/javascripts/controller/indexController.js
+++ public/javascripts/controller/indexController.js
@@ -1,14 +1,26 @@
 app.controller('indexController', ['$scope', 'indexFactory', ($scope, indexFactory) => {
 
-    const connectionOptions = {
-        reconnectionAttempts: 3,
-        reconnectionDelays: 600
+    $scope.init = () => {
+        const username = prompt('Please enter username');
+
+        if (username)
+            initSocket(username);
+        else
+            return false;
+    };
+
+    function initSocket(username) {
+        const connectionOptions = {
+            reconnectionAttempts: 3,
+            reconnectionDelays: 600
+        }
+    
+        indexFactory.connectSocket('http://localhost:3000', connectionOptions)
+        .then((socket) => {
+            socket.emit('newUser', { username });
+        }).catch((err) => {
+            console.log(err);
+        });
     }
 
-    indexFactory.connectSocket('http://localhost:3001', connectionOptions)
-    .then((socket) => {
-        console.log('Connection is established!', socket);
-    }).catch((err) => {
-        console.log(err);
-    });
 }]);
